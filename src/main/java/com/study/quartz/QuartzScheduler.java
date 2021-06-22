8176dbd1a4d8377340efe5996365b481c24b043a
xingcmy <67186152+xingcmy@users.noreply.github.com>
2021-06-22 20:53:28 +0800

    xingcmy

--- src/main/java/com/study/quartz/QuartzScheduler.java
+++ src/main/java/com/study/quartz/QuartzScheduler.java
@@ -1,19 +1,39 @@
 package com.study.quartz;
 
+import org.junit.Test;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.util.Date;
+
 public class QuartzScheduler {
     public static void main(String[] args) throws SchedulerException {
+        QuartzScheduler quartzScheduler=new QuartzScheduler();
+        quartzScheduler.test1();
+    }
+    public void test1() throws SchedulerException {
         //jobDetail 与job 绑定
-        JobDetail jobDetail= JobBuilder.newJob(QuartzJob.class).withIdentity("quartsDemo").build();
-        Trigger trigger = TriggerBuilder.newTrigger().withIdentity("quartzTrigger").startNow()
-                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(2).repeatForever())
+        JobDetail jobDetail= JobBuilder.newJob(QuartzJob.class)
+                .withIdentity("quartzDemo","1")  //创建标识
+                .usingJobData("msg1","1") //传入参数
+                .usingJobData("msg2","2")
                 .build();
+        //Trigger 触发 job 执行
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity("quartzTrigger","1") //创建标识
+                .startNow()  //立即执行
+                .usingJobData("msg1","01") //传入参数
+                .usingJobData("msg2","02")
+                //设置频率
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withIntervalInSeconds(10)  //每十秒执行
+                        .repeatForever())  //不间断重复执行
+                .build();
+        //Scheduler
         SchedulerFactory schedulerFactory = new StdSchedulerFactory();
         Scheduler scheduler = schedulerFactory.getScheduler();
         scheduler.start();
-        scheduler.scheduleJob(jobDetail,trigger);
+        scheduler.scheduleJob(jobDetail,trigger); //绑定JobDetail与Trigger
 
     }
 }
